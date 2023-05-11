bd8690026403594e857efd4a8648b28072b7b531
povilaszvaliauskas <povilas.zva@gmail.com>
2023-05-11 11:32:55 +0300

    add subscribe with seed only example

---
+++ examples/subscribe_with_seed.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"github.com/SyntropyNet/pubsub-go/pubsub"
+	"github.com/nats-io/nats.go"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	natsUrl                 = "nats://127.0.0.1"
+	natsUserSeed            = "SAAGNJOZTRPYYXG2NJX3ZNGXYUSDYX2BWO447W3SHG6XQ7U66RWHQ3JUXM"
+	exampleSubscribeSubject = "example.sub.subject"
+)
+
+// RepublishData receives a message on a given subject and republishes it to another subject.
+// It takes a context, the service instance, and the data (message) as input arguments.
+func PrintData(ctx context.Context, service *pubsub.NatsService, data []byte) error {
+	log.Println("Received message on", exampleSubscribeSubject, "subject")
+	return nil
+}
+
+func main() {
+	jwt, _ := pubsub.CreateAppJwt(natsUserSeed)
+	println(jwt)
+	// Set user credentials and options for NATS connection.
+	opts := []nats.Option{}
+	opts = append(opts, nats.UserJWTAndSeed(jwt, natsUserSeed))
+
+	// Connect to the NATS server using the provided options.
+	service := pubsub.MustConnect(
+		pubsub.Config{
+			URI:  natsUrl,
+			Opts: opts,
+		})
+	log.Println("Connected to NATS server.")
+
+	// Create a context with a cancel function to control the cancellation of ongoing operations.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Add a handler function to process messages received on the exampleSubscribeSubject.
+	service.AddHandler(exampleSubscribeSubject, func(data []byte) error {
+		return PrintData(ctx, service, data)
+	})
+
+	// Set up signal handling to gracefully shut down when receiving SIGINT or SIGTERM signals.
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signalChan
+		cancel()
+	}()
+
+	// Start serving messages and processing them using the registered handler function.
+	service.Serve(ctx)
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,10 @@ module github.com/SyntropyNet/pubsub-go
 
 go 1.19
 
-require (
-	github.com/nats-io/nats.go v1.24.0
-)
+require github.com/nats-io/nats.go v1.24.0
 
 require (
+	github.com/SyntropyNet/pubsub-go/pubsub v0.0.0-20230511082716-4a356f7c8189 // indirect
 	github.com/nats-io/nats-server/v2 v2.9.16 // indirect
 	github.com/nats-io/nkeys v0.4.4 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
