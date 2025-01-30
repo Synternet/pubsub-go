cae7ac815abf7e5c81a6bf628cdd62fbb09f1fc8
Gytis <gytis@noia.network>
2025-01-30 14:29:01 +0200

    Update of given example scripts.
    - subscribe_with_seed.go - printing actual incoming subject message
      instead of generic print
    - pubsub.go -> publish_with_jwt_and_seed.go: complete rework
    - pubusb_with_creds_file.go -> republish_using_creds_files.go: compete
      rework of republishing scenario due to deperecated actions

--- examples/publish_with_jwt_and_seed.go
+++ examples/publish_with_jwt_and_seed.go
@@ -6,23 +6,23 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"fmt"
+	"time"
 
 	"github.com/Synternet/pubsub-go/pubsub"
 	"github.com/nats-io/nats.go"
 )
 
 const (
-	natsUrl                 = "nats://127.0.0.1:4222"
+	natsUrl                 = "nats://127.0.0.1"
 	userCredsJWT            = "USER_JWT"
 	accessToken             = "EXAMPLE_ACCESS_TOKEN"
-	exampleSubscribeSubject = "example.sub.subject"
-	examplePublishSubject   = "example.pub.subject"
+	examplePublishSubject   = "publisher.example.subject"
 )
 
 // RepublishData receives a message on a given subject and republishes it to another subject.
 // It takes a context, the service instance, and the data (message) as input arguments.
-func RepublishData(ctx context.Context, service *pubsub.NatsService, data []byte) error {
-	log.Println("Received message on", exampleSubscribeSubject, "subject")
+func PublishData(ctx context.Context, service *pubsub.NatsService, data []byte) error {
 	err := service.Publish(ctx, examplePublishSubject, data)
 	if err != nil {
 		return err
@@ -32,6 +32,7 @@ func RepublishData(ctx context.Context, service *pubsub.NatsService, data []byte
 }
 
 func main() {
+
 	// Set user credentials and options for NATS connection.
 	opts := []nats.Option{}
 	opts = append(opts, nats.UserJWTAndSeed(userCredsJWT, accessToken))
@@ -48,10 +49,18 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Add a handler function to process messages received on the exampleSubscribeSubject.
-	service.AddHandler(exampleSubscribeSubject, func(data []byte) error {
-		return RepublishData(ctx, service, data)
-	})
+	// Publish messages in a continuous loop
+	value := 0
+	for {
+		msg := fmt.Sprintf("Value: %d", value)
+		if err := service.Publish(ctx, examplePublishSubject, []byte(msg)); err != nil {
+			log.Printf("Failed to publish message: %v", err)
+		} else {
+			fmt.Printf("Published message: %s\n", msg)
+		}
+		value++
+		time.Sleep(1 * time.Second)
+	}
 
 	// Set up signal handling to gracefully shut down when receiving SIGINT or SIGTERM signals.
 	signalChan := make(chan os.Signal, 1)
